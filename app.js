cd7e20084f5eb371391eb71e8477e2abbd8e27d5
Nirmal Jyoti Biswas <nirmal@macbookAir.local>
2022-01-04 01:40:58 +0530

    Added 3000 to port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require('ejs');
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
